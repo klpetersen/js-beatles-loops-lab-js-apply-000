4dadeb6f6fe9bc2d1c2e12651fc6e7feb907aa7e
Kayla Petersen <petersen1994@gmail.com>
2019-07-24 13:54:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) { 
   var music = [];
   for (var i = 0; i < musicians.length; i++) {
-    for (var j = 0; j < instruments.length; j++) { 
+     { 
       music += `${musicians[i]} plays ${instruments[j]} `
     }
   }
